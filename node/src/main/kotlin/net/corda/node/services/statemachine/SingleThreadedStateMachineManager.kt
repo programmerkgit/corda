dccf4f267da51fa2a563f963ed6b6e3743066453
nikinagy <nikolett.nagy@r3.com>
2020-06-12 10:26:44 +0100

    implementation: waiting for one network map refresh cycle in case of missing identity

--- node/src/main/kotlin/net/corda/node/services/statemachine/SingleThreadedStateMachineManager.kt
+++ node/src/main/kotlin/net/corda/node/services/statemachine/SingleThreadedStateMachineManager.kt
@@ -144,6 +144,7 @@ internal class SingleThreadedStateMachineManager(
         )
 
         val fibers = restoreFlowsFromCheckpoints()
+        flowHospital.startNetworkMapSubscribe()
         metrics.register("Flows.InFlight", Gauge<Int> { innerState.flows.size })
 
         setFlowDefaultUncaughtExceptionHandler()
@@ -733,7 +734,7 @@ internal class SingleThreadedStateMachineManager(
     private fun makeFlowHospital() : StaffedFlowHospital {
         // If the node is running as a notary service, we don't retain errored session initiation requests in case of missing Cordapps
         // to avoid memory leaks if the notary is under heavy load.
-        return StaffedFlowHospital(flowMessaging, serviceHub.clock, ourSenderUUID)
+        return StaffedFlowHospital(flowMessaging, serviceHub.clock, ourSenderUUID, serviceHub.networkMapCache, serviceHub.networkMapUpdater)
     }
 
     private fun StateMachineInnerState.removeFlowOrderly(
